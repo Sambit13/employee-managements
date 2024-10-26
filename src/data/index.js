69f505936f5529c3e48f2bcfa3343de92746cbcb
Sambit13 <sambitranjan0123@gmail.com>
2024-10-26 16:56:14 +0530

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Sambit',
+    lastName: 'Sahoo',
+    email: 'ssahoo@gmail.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Rohan',
+    lastName: 'Krovvidi',
+    email: 'ananta@gmail.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Rohit',
+    lastName: 'karukola',
+    email: 'rohit@gmail.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Vivek',
+    lastName: 'kannamreddi',
+    email: 'vivek.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Aswin',
+    lastName: 'Grant',
+    email: 'aswin@gmail.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'Praneeth',
+    lastName: 'pudi',
+    email: 'praneeth@gmail.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'michel',
+    lastName: 'jackson',
+    email: 'michel@example.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'hari',
+    lastName: 'pulakala',
+    email: 'hari@example.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'Jaggu',
+    lastName: 'jasoos',
+    email: 'jaggu@example.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'harry',
+    lastName: 'potter',
+    email: 'hary@example.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
